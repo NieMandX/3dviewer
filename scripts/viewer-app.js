ae06363d282ac48582f9a18096c573b5e6ffda50
maragojeep <maragojeep@gmail.com>
2025-10-31 16:39:28 +0300

    Polish background toggle

--- scripts/viewer-app.js
+++ scripts/viewer-app.js
@@ -504,7 +504,7 @@ class ViewerApp {
         let bgMesh = null; // background sphere used to show HDRI
         app.bgMesh = bgMesh;
         let bgMode = 'white';
-        const whiteClearColor = new THREE.Color().setRGB(1.5, 1.5, 1.5);
+        const whiteClearColor = new THREE.Color().setRGB(10, 10, 10);
 
         const camera   = new THREE.PerspectiveCamera(60, 1, 0.01, 5000);
         camera.position.set(2.5, 1.5, 3.5);
