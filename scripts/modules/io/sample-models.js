aa0e7581e84ae123ed689f340e0c6f9aa3455f02
maragojeep <maragojeep@gmail.com>
2026-03-05 02:33:56 +0300

    fix(samples): map SH34/SH35 LPM/HPM to current maragojeep files

--- scripts/modules/io/sample-models.js
+++ scripts/modules/io/sample-models.js
@@ -1,29 +1,31 @@
+const YC_BUCKET_BASE_URL = 'https://storage.yandexcloud.net/maragojeep';
+
 export const SAMPLE_MODELS = [
     { label: 'Примеры моделей', files: [] },
     {
         label: 'SH35_LPM',
         files: [
-            'https://storage.yandexcloud.net/maragojeep/0610_Shabolovka_Vl_35.zip?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=YCAJENUFHbvNEEcd7Rb00AGxU%2F20250923%2Fru-central1%2Fs3%2Faws4_request&X-Amz-Date=20250923T230730Z&X-Amz-Expires=2592000&X-Amz-Signature=943e5ff00396c1cc7f942e434853be47d68d7a31d6bcd346e6c191b8e6c6d157&X-Amz-SignedHeaders=host'
+            `${YC_BUCKET_BASE_URL}/0610_Shabolovka_Vl_35.zip`,
         ]
     },
     {
         label: 'SH34_LPM',
         files: [
-            'https://storage.yandexcloud.net/maragojeep/0610_Shabolovka_Vl_34.zip?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=YCAJENUFHbvNEEcd7Rb00AGxU%2F20250923%2Fru-central1%2Fs3%2Faws4_request&X-Amz-Date=20250923T230545Z&X-Amz-Expires=2592000&X-Amz-Signature=d25d916a2754c41a582a7618cee65834fcfb4931f70f0ba583c625b617c20430&X-Amz-SignedHeaders=host'
+            `${YC_BUCKET_BASE_URL}/0610_Shabolovka_Vl_34.zip`,
         ]
     },
     {
         label: 'SH35_HPM',
         files: [
-            'https://storage.yandexcloud.net/maragojeep/SM_Shabolovka_Vl_35.zip?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=YCAJENUFHbvNEEcd7Rb00AGxU%2F20250923%2Fru-central1%2Fs3%2Faws4_request&X-Amz-Date=20250923T230932Z&X-Amz-Expires=2592000&X-Amz-Signature=6419e24698888a213131664bdee893f90b07fd79d5dc46ec3db66bcc5862f6f6&X-Amz-SignedHeaders=host',
-            'https://storage.yandexcloud.net/maragojeep/SM_Shabolovka_Vl_35_Ground.zip?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=YCAJENUFHbvNEEcd7Rb00AGxU%2F20250923%2Fru-central1%2Fs3%2Faws4_request&X-Amz-Date=20250923T231155Z&X-Amz-Expires=2592000&X-Amz-Signature=826ed6e3fb7b07c9ac490396cabac4acbfd284d4c41d3e77786934f10318a7bb&X-Amz-SignedHeaders=host'
+            `${YC_BUCKET_BASE_URL}/SM_Shabolovka_Vl_35.zip`,
+            `${YC_BUCKET_BASE_URL}/SM_Shabolovka_Vl_35_Ground.zip`,
         ]
     },
     {
         label: 'SH34_HPM',
         files: [
-            'https://storage.yandexcloud.net/maragojeep/SM_Shabolovka_Vl_34.zip?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=YCAJENUFHbvNEEcd7Rb00AGxU%2F20250923%2Fru-central1%2Fs3%2Faws4_request&X-Amz-Date=20250923T230806Z&X-Amz-Expires=2592000&X-Amz-Signature=47cf7ad4a3548de434900644f8de1bedc24facda64553d58c823bab2ff349844&X-Amz-SignedHeaders=host',
-            'https://storage.yandexcloud.net/maragojeep/SM_Shabolovka_Vl_34_Ground.zip?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=YCAJENUFHbvNEEcd7Rb00AGxU%2F20250923%2Fru-central1%2Fs3%2Faws4_request&X-Amz-Date=20250923T230833Z&X-Amz-Expires=2592000&X-Amz-Signature=bdf7d9cb9ca7ede1344ff0b89b59d54768fdaa1ad7810f87136c39f9ec61c017&X-Amz-SignedHeaders=host'
+            `${YC_BUCKET_BASE_URL}/SM_Shabolovka_Vl_34.zip`,
+            `${YC_BUCKET_BASE_URL}/SM_Shabolovka_Vl_34_Ground.zip`,
         ]
     }
 ];
