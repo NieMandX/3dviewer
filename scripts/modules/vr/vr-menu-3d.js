74261642eebb50a270bc2b2ba1ff750a0e348014
maragojeep <maragojeep@gmail.com>
2026-03-12 06:38:35 +0300

    Revert "Use controller grip as VR menu ray origin"
    This reverts commit 95b5589187e87a29a343e90ca635807562ccfdd1.

--- scripts/modules/vr/vr-menu-3d.js
+++ scripts/modules/vr/vr-menu-3d.js
@@ -247,22 +247,13 @@ export function createVRMenu3D(options = {}) {
 
         for (let i = 0; i < 2; i += 1) {
             const controller = renderer.xr.getController(i);
-            const grip = typeof renderer?.xr?.getControllerGrip === 'function'
-                ? renderer.xr.getControllerGrip(i)
-                : null;
             if (!controller) continue;
             if (!controller.parent) {
                 scene.add(controller);
                 controller.userData.__vrMenuAttachedToScene = true;
             }
-            if (grip && !grip.parent) {
-                scene.add(grip);
-                grip.userData.__vrMenuAttachedToScene = true;
-            }
-
-            const rayNode = grip || controller;
             const line = createLine(THREE);
-            rayNode.add(line);
+            controller.add(line);
 
             const onConnected = (event) => {
                 controller.userData.inputSource = event?.data || null;
@@ -276,8 +267,6 @@ export function createVRMenu3D(options = {}) {
 
             state.controllers.push({
                 controller,
-                grip,
-                rayNode,
                 line,
                 onConnected,
                 onDisconnected,
@@ -387,13 +376,13 @@ export function createVRMenu3D(options = {}) {
     }
 
     function computeControllerHit(controllerData) {
-        const rayObject = controllerData?.rayNode || controllerData?.controller || null;
-        if (!rayObject) return null;
+        const controller = controllerData?.controller || null;
+        if (!controller) return null;
 
-        rayObject.updateWorldMatrix(true, false);
-        rayOrigin.setFromMatrixPosition(rayObject.matrixWorld);
+        controller.updateWorldMatrix(true, false);
+        rayOrigin.setFromMatrixPosition(controller.matrixWorld);
         rayDir.set(0, 0, -1);
-        rayDir.transformDirection(rayObject.matrixWorld);
+        rayDir.transformDirection(controller.matrixWorld);
 
         raycaster.near = 0;
         raycaster.far = MAX_RAY_DISTANCE;
@@ -459,18 +448,14 @@ export function createVRMenu3D(options = {}) {
         hide();
 
         for (const data of state.controllers) {
-            if (data?.rayNode && data?.line) {
-                data.rayNode.remove(data.line);
+            if (data?.controller && data?.line) {
+                data.controller.remove(data.line);
                 data.line.geometry?.dispose?.();
                 data.line.material?.dispose?.();
             }
             if (data?.controller?.userData?.__vrMenuAttachedToScene && data.controller.parent === scene) {
                 scene.remove(data.controller);
                 delete data.controller.userData.__vrMenuAttachedToScene;
-            }
-            if (data?.grip?.userData?.__vrMenuAttachedToScene && data.grip.parent === scene) {
-                scene.remove(data.grip);
-                delete data.grip.userData.__vrMenuAttachedToScene;
             }
             if (data?.controller && data?.onConnected) {
                 data.controller.removeEventListener('connected', data.onConnected);
