eb808f4988de379a0c192d95a5b632c75b85c254
maragojeep <maragojeep@gmail.com>
2026-09-01 23:35:19 +0300

    Switch viewer to Yandex Supabase

--- config/runtime.js
+++ config/runtime.js
@@ -1,5 +1,5 @@
 window.__LPMVIEW_RUNTIME = Object.assign({}, window.__LPMVIEW_RUNTIME || {}, {
-  supabaseUrl: 'https://xlajjnmxvlkygixamyam.supabase.co',
-  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhsYWpqbm14dmxreWdpeGFteWFtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjY0NjY2MzEsImV4cCI6MjA4MjA0MjYzMX0.02uAo7lQq8UNVB3yL45WudQ8WSR4opNlZ2frTZNRl8Q',
+  supabaseUrl: 'https://supabase.agr.vision',
+  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlzcyI6InN1cGFiYXNlIiwiaWF0IjoxNzg4MjY2NzAwLCJleHAiOjE5NDU5NDY3MDB9.WZ3JGCeU_xEMeSvRaSP9RsqQcJVPXcFrZrWNbHKI048',
   voiceApiUrl: 'https://voice-api.agr.vision'
 });
